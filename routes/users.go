be7ca8aea05443450ce9d15c986d99282cc0f18c
hamid-JTG <hamid.husain@joshtechnologygroup.com>
2025-03-03 13:59:19 +0530

    login ini

--- routes/users.go
+++ routes/users.go
@@ -20,5 +20,8 @@ func APIRoutes(router *gin.Engine, service *services.Service) {
 	protected.Use(middleware.AuthMiddleware())
 	{
 		protected.GET("/logout", controllers.Logout)
+		users.GET("/", controllers.ListUsers)
+		users.POST("/create", controllers.CreateUser)
+		users.POST("/login", controllers.Login)
 	}
 }
