a8a99be1ee5ae05e6121664fcb9c0b28da3340d7
hamid-JTG <hamid.husain@joshtechnologygroup.com>
2025-03-03 15:50:07 +0530

    login

--- controllers/auth.go
+++ controllers/auth.go
@@ -34,3 +34,31 @@ func CreateUser(service *services.Service) gin.HandlerFunc {
 		c.JSON(http.StatusCreated, response)
 	}
 }
+
+func Login(c *gin.Context) {
+	var req serializers.LoginRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, utils.ParseValidationErrors(err))
+		return
+	}
+
+	user, err := services.VerifyUserCredentials(req.Email, req.Password)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
+		return
+	}
+
+	token, expiryTime, err := utils.GenerateJWT(user.ID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate token"})
+		return
+	}
+
+	userResponse := serializers.SerializeLoginResponse(*user)
+	tokenResponse := serializers.SerializeToken(token, expiryTime)
+	c.JSON(http.StatusOK, gin.H{
+		"user_details": userResponse,
+		"token":        tokenResponse,
+	})
+}
