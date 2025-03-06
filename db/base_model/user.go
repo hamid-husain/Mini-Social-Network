6f38b87615500f2f3a833d2dbce4a81364c06a76
hamid-JTG <hamid.husain@joshtechnologygroup.com>
2025-03-06 11:45:41 +0530

    minor-fix

--- db/base_model/user.go
+++ db/base_model/user.go
@@ -8,8 +8,8 @@ type User struct {
 	FirstName          string              `gorm:"type:varchar(46); not null"`
 	LastName           string              `gorm:"type:varchar(46)"`
 	DateOfBirth        string              `gorm:"type:date; not null"`
-	Gender             uint8               `gorm:"type:smallint;check:gender IN (1,2,3)"`
-	MaritalStatus      uint8               `gorm:"type:smallint;check:marital_status IN (1,2)"`
+	Gender             uint8               `gorm:"type:smallint; not null"`
+	MaritalStatus      uint8               `gorm:"type:smallint; not null"`
 	Password           string              `gorm:"type:varchar(255); not null"`
 	OfficeDetails      []OfficeDetail      `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
 	ResidentialDetails []ResidentialDetail `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
--- models/user.go
+++ models/user.go
@@ -5,13 +5,13 @@ import "gorm.io/gorm"
 type User struct {
 	gorm.Model
 	Email              string              `gorm:"type:varchar(254);uniqueIndex:uniq_email,WHERE:deleted_at IS NULL; not null"`
-	FirstName          string              `gorm:"type:varchar(46); not null"`
-	LastName           string              `gorm:"type:varchar(46)"`
+	FirstName          string              `gorm:"type:varchar(48); not null"`
+	LastName           string              `gorm:"type:varchar(48)"`
 	DateOfBirth        string              `gorm:"type:date; not null"`
-	Gender             uint8               `gorm:"type:smallint;check:gender IN (1,2,3)"`
-	MaritalStatus      uint8               `gorm:"type:smallint;check:marital_status IN (1,2)"`
+	Gender             uint8               `gorm:"type:smallint; not null"`
+	MaritalStatus      uint8               `gorm:"type:smallint; not null"`
 	Password           string              `gorm:"type:varchar(255); not null"`
-	OfficeDetails      []OfficeDetail      `gorm:"foreignKey:UserID"`
-	ResidentialDetails []ResidentialDetail `gorm:"foreignKey:UserID"`
+	OfficeDetails      []OfficeDetail      `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
+	ResidentialDetails []ResidentialDetail `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
 	Followers          []User              `gorm:"many2many:user_followers;joinForeignKey:UserID;joinReferences:FollowerID"`
 }
